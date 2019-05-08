13016fe3c2bc218ae7210a84e98f44ae669c5913
michael murray <murraymd1@gmail.com>
2019-05-08 14:52:07 -0700

    ninja gold

--- apps/ninja_gold/views.py
+++ apps/ninja_gold/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 import random
 
 # Create your views here.
@@ -12,15 +12,15 @@ def index(request):
 
 def gold(request):
     cash = {
-    'Metal Detecting':random.randint(10,20),
-    'Back Alley Dice':random.randint(5,10),
-    'Couch Cusion Raiding':random.randint(2,5),
-    'Casino':random.randint(-50,50),
+    'Metal Detecting Adventure':random.randint(10,20),
+    'Back Alley Dice game':random.randint(5,10),
+    'Couch Cusion Raid':random.randint(2,5),
+    'Casino Card Game':random.randint(-50,50),
     }
     request.session['gold']+=cash[request.POST['building']]
     if cash[request.POST['building']] > 0:
         activity={
-            'text': f"you went/entered a {request.POST['building']} and gained {cash[request.POST['building']]} gold",
+            'text': f"you entered a {request.POST['building']} and gained {cash[request.POST['building']]} gold",
             'color': "green"
         }
     else:
@@ -34,5 +34,6 @@ def gold(request):
 
 def reset_gold(request):
     del request.session['gold']
+    del request.session['activities']
     return redirect('/ninja_gold/')
 
